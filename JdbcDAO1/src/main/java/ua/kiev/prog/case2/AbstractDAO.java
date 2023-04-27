71a62a243431bff08c5b6c97ae3d48827aed3bb2
VladOstapchuk <vlad.ostapchuk@gmail.com>
2023-04-28 00:10:13 +0300

    Initial commit

---
+++ JdbcDAO1/src/main/java/ua/kiev/prog/case2/AbstractDAO.java
@@ -0,0 +1,239 @@
+package ua.kiev.prog.case2;
+
+import ua.kiev.prog.shared.Id;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractDAO<T> {
+    private final Connection conn;
+    private final String table;
+    private ResultSetMetaData md;
+
+    public AbstractDAO(Connection conn, String table) {
+        this.conn = conn;
+        this.table = table;
+    }
+
+    public void createTable(Class<T> cls) {
+        Field[] fields = cls.getDeclaredFields();
+        Field id = getPrimaryKeyField(null, fields);
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("CREATE TABLE ")
+                .append(table)
+                .append("(");
+
+        sql.append(id.getName())
+                .append(" ")
+                .append(" INT AUTO_INCREMENT PRIMARY KEY,");
+
+        for (Field f : fields) {
+            if (f != id) {
+                f.setAccessible(true);
+
+                sql.append(f.getName()).append(" ");
+
+                if (f.getType() == int.class) {
+                    sql.append("INT,");
+                } else if (f.getType() == String.class) {
+                    sql.append("VARCHAR(100),");
+                } else
+                    throw new RuntimeException("Wrong type");
+            }
+        }
+
+        sql.deleteCharAt(sql.length() - 1);
+        sql.append(")");
+
+        try {
+            try (Statement st = conn.createStatement()) {
+                st.execute(sql.toString());
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void add(T t) {
+        try {
+            Field[] fields = t.getClass().getDeclaredFields();
+            Field id = getPrimaryKeyField(t, fields);
+
+            StringBuilder names = new StringBuilder();
+            StringBuilder values = new StringBuilder();
+
+            // insert into t (name,age) values("..",..
+
+            for (Field f : fields) {
+                if (f != id) {
+                    f.setAccessible(true);
+
+                    names.append(f.getName()).append(',');
+                    values.append('"').append(f.get(t)).append("\",");
+                }
+            }
+
+            names.deleteCharAt(names.length() - 1); // last ','
+            values.deleteCharAt(values.length() - 1);
+            String sql = "INSERT INTO " + table + "(" + names.toString() +
+                    ") VALUES(" + values.toString() + ")";
+
+            try (Statement st = conn.createStatement()) {
+                st.execute(sql);
+                String[] valuesT = values.toString().split(",");
+                ResultSet rs = st.executeQuery("SELECT id from " +
+                        table + " WHERE name = " + valuesT[0] + " AND age = " +
+                        valuesT[1]
+                );
+                id.setAccessible(true);
+                if (rs.next()) {
+                    id.set(t, rs.getInt(1));
+                }
+            }
+
+            // TODO: get ID
+            // SELECT - X
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void update(T t) {
+        try {
+            Field[] fields = t.getClass().getDeclaredFields();
+            Field id = getPrimaryKeyField(t, fields);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (Field f : fields) {
+                if (f != id) {
+                    f.setAccessible(true);
+
+                    sb.append(f.getName())
+                            .append(" = ")
+                            .append('"')
+                            .append(f.get(t))
+                            .append('"')
+                            .append(',');
+                }
+            }
+
+            sb.deleteCharAt(sb.length() - 1);
+
+            // update t set name = "aaaa", age = "22" where id = 5
+            String sql = "UPDATE " + table + " SET " + sb.toString() + " WHERE " +
+                    id.getName() + " = \"" + id.get(t) + "\"";
+
+            try (Statement st = conn.createStatement()) {
+                st.execute(sql);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void delete(T t) {
+        try {
+            Field[] fields = t.getClass().getDeclaredFields();
+            Field id = getPrimaryKeyField(t, fields);
+
+            String sql = "DELETE FROM " + table + " WHERE " + id.getName() +
+                    " = \"" + id.get(t) + "\"";
+
+            try (Statement st = conn.createStatement()) {
+                st.execute(sql);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public List<T> getAll(Class<T> cls) {
+        List<T> res = new ArrayList<>();
+
+        try {
+            try (Statement st = conn.createStatement()) {
+                try (ResultSet rs = st.executeQuery("SELECT * FROM " + table)) {
+                    ResultSetMetaData md = rs.getMetaData();
+
+                    while (rs.next()) {
+                        T t = cls.getDeclaredConstructor().newInstance(); //!!!
+
+                        for (int i = 1; i <= md.getColumnCount(); i++) {
+                            String columnName = md.getColumnName(i);
+                            Field field = cls.getDeclaredField(columnName);
+                            field.setAccessible(true);
+
+                            field.set(t, rs.getObject(columnName));
+                        }
+
+                        res.add(t);
+                    }
+                }
+            }
+
+            return res;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private Field getPrimaryKeyField(T t, Field[] fields) {
+        Field result = null;
+
+        for (Field f : fields) {
+            if (f.isAnnotationPresent(Id.class)) {
+                result = f;
+                result.setAccessible(true);
+                break;
+            }
+        }
+
+        if (result == null)
+            throw new RuntimeException("No Id field found");
+
+        return result;
+    }
+
+
+    public List<T> getAll(Class<T> cls, String... column) {
+        List<T> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < column.length; i++) {
+            if (i > 0)
+                sb.append(",");
+            sb.append(column[i]);
+        }
+
+        try {
+            try (Statement st = conn.createStatement()) {
+
+                try (ResultSet rs = st.executeQuery("SELECT " + sb + " FROM " + table)) {
+                    ResultSetMetaData md = rs.getMetaData();
+
+                    while (rs.next()) {
+                        T t = cls.getDeclaredConstructor().newInstance(); //!!!
+
+                        for (int i = 1; i <= md.getColumnCount(); i++) {
+                            String columnName = md.getColumnName(i);
+                            Field field = cls.getDeclaredField(columnName);
+                            field.setAccessible(true);
+
+                            field.set(t, rs.getObject(columnName));
+                        }
+                        res.add(t);
+                    }
+                }
+            }
+
+            return res;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
